28fc8938873b38fa27b21cea83fd41161ba38fdf
christeefy <fuyuan.tee@mail.utoronto.ca>
2019-01-22 12:31:12 -0500

    Add face blurring functionality

--- auto_anonymize/utils/viz.py
+++ auto_anonymize/utils/viz.py
@@ -29,7 +29,7 @@ def mark_faces_plt(img_path, bboxes):
     return fig
 
 
-def mark_faces_cv2(img, bboxes):
+def mark_faces_cv2(frame, bboxes):
     '''
     Apply bounding boxes around faces using cv2.
 
@@ -42,8 +42,44 @@ def mark_faces_cv2(img, bboxes):
     if not len(bboxes): return
     for bbox in bboxes:
         x1, y1, w, h, _ = np.split(bbox, [1, 2, 3, 4])
-        cv2.rectangle(img,
+        cv2.rectangle(frame,
                       (x1, y1), 
                       (x1 + w, y1 + h), 
                       (255, 0, 0), 
                       2)
+
+
+def blur_faces(frame, bboxes, blur_mode='gaussian', *args):
+    '''
+    Blur faces in a cv2 image. 
+
+    Inputs:
+        frame: A cv2 image
+        bboxes: Bounding boxes coordinates 
+                for faces (x1, y1, w, h)
+        blur_mode: A string denoting blurring method.
+                   Valid values: {'gaussian', 'median'}
+
+    Returns:
+        A cv2 image with the same properties of `frame` 
+        with faces blurred.
+    '''
+    blur_func_key = {
+        'gaussian': cv2.GaussianBlur,
+        'median': cv2.medianBlur
+    }
+
+    assert blur_mode in blur_func_key.keys()
+
+    if not len(bboxes): return frame
+
+    for bbox in bboxes:
+        x_slice = slice(bbox[0], bbox[0] + bbox[2])
+        y_slice = slice(bbox[1], bbox[1] + bbox[3])
+
+        face = frame[y_slice, x_slice]
+        face = blur_func_key[blur_mode](face, *args)
+
+        frame[y_slice, x_slice] = face
+
+    return frame
