111239e943f282726c9e58e962a2976e5e005e4a
christeefy <fuyuan.tee@mail.utoronto.ca>
2019-03-04 12:59:11 -0500

    Decouple webapp from main repo.

--- flask/webapp/static/js/video_form_feedback.js
+++
@@ -1,7 +0,0 @@
-const videoFile = document.getElementById('videoFile')
-const videoFeedback = document.getElementById('videoFeedback')
-
-videoFile.addEventListener('change', e => {
-    e.preventDefault()
-    videoFeedback.innerText = videoFile.files[0].name
-})
